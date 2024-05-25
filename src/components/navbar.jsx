eed2d9ec2fa64032a9f22a4fb168adcf277e4b0d
AsilbekMelikov <melikovasilbek04@gmail.com>
2024-05-25 10:59:33 +0500

    Change the ts logo color and add navigation links for smartphone responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -39,8 +39,21 @@ const Navbar = () => {
         <div
           className={`${
             !toggleNav ? "hidden" : "flex"
-          } p-6 absolute top-20 right-0 left-0 w-full sidebar bg-black bg-black-gradient`}
-        ></div>
+          } p-6 absolute top-20 right-0 left-0 w-full sidebar bg-black-gradient `}
+        >
+          <ul className={`list-none ${styles.flexCenter} flex-1`}>
+            {navigationLinks.map((link, idx) => {
+              return (
+                <li
+                  key={link.id}
+                  className="font-montserrat font-normal cursor-pointer text-[16px] text-white mr-10 last:mr-0 hover:text-lightWhite duration-150"
+                >
+                  {link.title}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
